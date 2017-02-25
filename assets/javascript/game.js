e8a8ac5b8d7e394047d1d3c2ba0bd21116423422
Jeff McMahan <Jeff McMahan>
2017-02-24 18:17:07 -0600

    added js code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,23 +1,84 @@
 var count = 11;
-
 var counter=setInterval(timer, 1000);
+var index = 0;
+
+var questionArray = [
+{
+	question:"What is the most visited National Park in the US? ",
+	choices:["Acadia","Grand Canyon","Smoky Mountain","Yellow Stone"],
+	correctChoice: 2
+},{
+	question:"Which of the following exists within the boundries of a national park?",
+	choices:["The highest point in North America","The longest cave system in the world","The deepest lake in the world","All of the above"],
+	correctChoice: 3
+},{
+	question:"Which national park has banned nearly all vehicles from its roads during peak season?",
+	choices:["Joshua Tree","Olympic","Yosemite","Zion"],
+	correctChoice: 3
+},{
+	question:"What is the newest national park?",
+	choices:["Congaree","Big Bend","Mount Rainier","Great Sand Dunes"],
+	correctChoice: 3
+},{
+	question:"Which of the following presidents more than doubled the acreage of the National Park System?",
+	choices:["Calvin Coolidge","Richard Nixion","Jimmy Carter","George H. Bush"],
+	correctChoice: 2
+}
+];
+
+console.log(questionArray);
+console.log(questionArray[0].choices[0]);
 
 function timer(){
-	count = count-1;
-	console.log(count);
-	if (count <= -1){
+	count--;
+	if (count < 0){
 		clearInterval(counter);
 		return;
-		console.log(count);
-	} else if (count == -1){
-		$("timerSays").html("Time Out You Suck!");
+	} else if (count == 0){
+		$("#timerSays").html("<h1>Time Out!</h1>");
 	}
-	$("#countDown").html(count);
-	console.log(count);
-	// if (count == ){
-	// 	$("timerSays").html("Time Out You Suck!");
-	// }
+	$("#countDown").html("<h1>Time Left: " + count + "</h1");
 }
 
-timer();
-console.log(count)
+function displayQuestions(){
+	for(var i = 0; i < questionArray.length; i++){
+		$("#questions").append("<h2>" + (i + 1) + ": " + questionArray[i].question + "</h2>");
+		var rButtons = questionAnswer()
+		$("#questions").append(rButtons);
+		timer();
+	}
+}
+
+function questionAnswer(index){
+	var answerList = $("<ul>");
+	var item;
+	var tag = '';
+	console.log(item);	
+	for (var i = 0; i < questionArray.length; i++){
+		for (var a = 0; a <questionArray[i].choices; a++){
+		var xChoice = questionArray[i].choices;
+		console.log(xChoice);
+		// var xChoice = questionArray[0].choices;
+		// console.log(xChoice);
+		// for (var x = 0; x < xChoice.length; x++){
+		// 	console.log(xChoice);
+			item = $("<li class='radio-inline'>");
+			tag = '<input type ="radio" name = "answer" value ="' + a + '"/>';
+			tag += xChoice[a];
+			item.append(tag);
+			answerList.append(item);
+			}
+		// console.log(item)
+		// console.log(tag)
+		// console.log(answerList)
+	}
+	return answerList;
+}
+
+
+
+
+window.onload = function(){
+// timer();
+displayQuestions();
+};
