2bce1fd0a42902a6b02e27f8ce1c0594ae96b140
Jeff McMahan <Jeff McMahan>
2017-02-25 22:58:56 -0600

    all js logic, css & html working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -26,91 +26,106 @@ var questionArray = [
 	question:"Which of the following presidents more than doubled the acreage of the National Park System?",
 	choices:["Calvin Coolidge","Richard Nixion","Jimmy Carter","George H. Bush"],
 	correctChoice: 2
+},{
+	question:"What is the smalles national park?",
+	choices:["Dry Tortugas","Hot Springs","Petrified Forest","Black Canyon Gunnison"],
+	correctChoice: 1
+},{
+	question:"What is the emblem of the National Park Service?",
+	choices:["Arrowhead","Diamond","Log cabin","Redwood tree"],
+	correctChoice: 0
 }
 ];
 
-console.log(questionArray);
-// console.log(questionArray[0].choices[0]);
+function clearTimer(){
+	count = 121;
+	counter = setInterval(timer, 1000);
+	$("#timerSays").html("");
+}
 
 function timer(){
 	count--;
 	if (count < 0){
 		clearInterval(counter);
 		return;
-	}else if (count == 0){
-		$("#timerSays").html("<h1>Time's Up!</h1>");
-		for (var i = 0; i < questionArray.length; i++){
-		var userChoice =  $('input:radio[name=answer' + i + ']:checked').val();
-		console.log(userChoice);
-		if (userChoice == "undefined"){
-			unanswered++;
-		}
-		else if (userChoice != questionArray[i].correctChoice){
-			incorrect++;
-		}
-		else{
-			correct++;
-		}
 	}
-		$("#timerDiv").hide('slow');
-		$("#questionsDiv").hide('slow');
-		$("#submit-quiz").hide('slow');
-		$("#summary").html("<h2>Correct answers: " + correct + "</h2>");
-		$("#summary").append("<h2>Incorrect answers: " + incorrect + "</h2>");
-		$("#summary").append("<h2>Unanswered Questions: " + unanswered + "</h2>");
+	else if (count == 0){
+		$("#timerSays").html("<h1>Time's Up!</h1>");
+			for (var i = 0; i < questionArray.length; i++){
+				var userChoice =  $('input:radio[name=answer' + i + ']:checked').val();
+				if (userChoice == undefined){
+					unanswered++;
+				}
+				else if (userChoice != questionArray[i].correctChoice){
+					incorrect++;
+				}
+				else{
+					correct++;
+				}
+			}
+		endGame();
 	};
 	$("#countDown").html("<h1>Time Left: " + count + "</h1");
+	console.log(correct);
+	console.log(incorrect);
 };
 
-
-
 function displayQuestion(){
-	// debugger;
 	for (var i = 0; i < questionArray.length; i++){
-		console.log(i);
-		$("#questions").append("<h2>" + questionArray[i].question + "</h2>");
-		console.log(questionArray);
-
+	$("#questions").append("<h2>" + questionArray[i].question + "</h2>");
 		for (var x = 0; x < questionArray[i].choices.length; x++){
 			var tChoice = questionArray[i].choices[x];
-			console.log(tChoice);
 			$("#questions").append("<ul>"+"<li class='radio-inline'>" + '<input type ="radio" name = "answer' + i + '" value ="' + x + '"/>' + tChoice +"</ul>")
 		}
 	}
+	$("#submit-quiz").on("click", function(event){
+		event.preventDefault();
+		clearInterval(counter);
+		for (var i = 0; i < questionArray.length; i++){
+			var userChoice =  $('input:radio[name=answer' + i + ']:checked').val();
+			if (userChoice == undefined){
+				unanswered++;
+			}
+			else if (userChoice != questionArray[i].correctChoice){
+				incorrect++;
+			}
+			else{
+				correct++;
+			}
+		}
+		endGame();	
+	});
 };
 
-$("#submit-quiz").on("click", function(event){
-	event.preventDefault();
-	
-	for (var i = 0; i < questionArray.length; i++){
-		var userChoice =  $('input:radio[name=answer' + i + ']:checked').val();
-		console.log(userChoice);
-		if (userChoice == "undefined"){
-			unanswered++;
-		}
-		else if (userChoice != questionArray[i].correctChoice){
-			incorrect++;
-		}
-		else{
-			correct++;
-		}
-	}
-	console.log(correct);
-	console.log(incorrect);
-	console.log(unanswered);
-
-
-
+function endGame(){
 	$("#timerDiv").hide('slow');
 	$("#questionsDiv").hide('slow');
+	$("#questions").html("")
 	$("#submit-quiz").hide('slow');
+	$("#summary").show();
 	$("#summary").html("<h2>Correct answers: " + correct + "</h2>");
 	$("#summary").append("<h2>Incorrect answers: " + incorrect + "</h2>");
-	$("#summary").append("<h2>Unanswered Questions: " + unanswered + "</h2>");
+	$("#summary").append("<h2>Unanswered Questions: " + unanswered + "</h2>")	
+	restartButton();
+}
 
-
-
-})
+function restartButton(){
+	var restart = $("<input class='btn btn-primary btn-block' type='button' id='restart' value='Play Again' />");
+	restart.appendTo($("#summary"));
+	$("#restart").on("click", function(event){
+		event.preventDefault();	
+		$("#summary").hide();
+		$("#timerDiv").show();
+		$("#questionsDiv").show();
+		$("#submit-quiz").show();
+		clearTimer();
+		displayQuestion();
+		timer();
+	});
+	correct = 0;
+	incorrect = 0;
+	unanswered = 0;
+};
 
 window.onload = function(){
 timer();
